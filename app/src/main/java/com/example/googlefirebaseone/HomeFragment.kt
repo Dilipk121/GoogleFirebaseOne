96c145668395ca5ad1f84f64ba110ec1b970b840
AVIATOR <dilip.web7@gmail.com>
2024-03-15 23:30:23 +0530

    final update

--- app/src/main/java/com/example/googlefirebaseone/HomeFragment.kt
+++ app/src/main/java/com/example/googlefirebaseone/HomeFragment.kt
@@ -5,14 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.googlefirebaseone.adapter.RvContactAdapter
 import com.example.googlefirebaseone.databinding.FragmentHomeBinding
+import com.example.googlefirebaseone.model.Contacts
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class HomeFragment : Fragment() {
 
     private var _binding : FragmentHomeBinding? = null
     private val binding  get() = _binding!!
+
+    private lateinit var contactList : ArrayList<Contacts>
+    private lateinit var firebaseRef : DatabaseReference
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,8 +40,43 @@ class HomeFragment : Fragment() {
         }
 
 
+        //fetch the data from firebase DB
+        firebaseRef = FirebaseDatabase.getInstance().getReference("contacts") // write same name as in DB
+        contactList = arrayListOf()
+
+
+        fetchFirebaseData() // fetch data function
+
+        binding.recyclerView.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(this.context)
+        }
+
 
         return binding.root
+    }
+
+    private fun fetchFirebaseData() {
+
+        firebaseRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                contactList.clear()
+                if (snapshot.exists()){
+                    for (contactSnap in snapshot.children){
+                        val contacts = contactSnap.getValue(Contacts::class.java)
+                        contactList.add(contacts!!)
+                    }
+                }
+                val rvAdapter = RvContactAdapter(contactList)
+                binding.recyclerView.adapter = rvAdapter //adapter attached to recycler view
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(context, "Error $error", Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
     }
 
 
---
+++ app/src/main/java/com/example/googlefirebaseone/adapter/RvContactAdapter.kt
@@ -0,0 +1,87 @@
+package com.example.googlefirebaseone.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.example.googlefirebaseone.HomeFragment
+import com.example.googlefirebaseone.HomeFragmentDirections
+import com.example.googlefirebaseone.databinding.RvContactItemsBinding
+import com.example.googlefirebaseone.model.Contacts
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.firebase.database.FirebaseDatabase
+
+class RvContactAdapter(private val contactList: ArrayList<Contacts>) :
+    RecyclerView.Adapter<RvContactAdapter.RvViewHolder>() {
+
+    class RvViewHolder(val binding: RvContactItemsBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvViewHolder {
+        return RvViewHolder(
+            RvContactItemsBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return contactList.size
+    }
+
+    override fun onBindViewHolder(holder: RvViewHolder, position: Int) {
+        val currentItem = contactList[position]
+
+        holder.apply {
+            binding.apply {
+                tvId.text = currentItem.id
+                tvName.text = currentItem.name
+                tvMobile.text = currentItem.mobile
+
+                rvContainer.setOnClickListener {
+
+                    val action = HomeFragmentDirections.actionHomeFragmentToUpdateFragment(
+                        currentItem.id.toString(),
+                        currentItem.name.toString(),
+                        currentItem.mobile.toString()
+                    )
+                    Navigation.findNavController(holder.itemView).navigate(action)
+
+                }
+
+                rvContainer.setOnLongClickListener {
+
+                    //alert box
+                    MaterialAlertDialogBuilder(holder.itemView.context)
+                        .setTitle("Delete Item Permanently")
+                        .setMessage("Are You Sure To Delete This Item")
+                        .setPositiveButton("Yes"){_,_ ->
+
+                        val firebaseRef = FirebaseDatabase.getInstance().getReference("contacts")
+                            firebaseRef.child(currentItem.id.toString()).removeValue()
+                                .addOnSuccessListener {
+                                    Toast.makeText(holder.itemView.context, "Item Removed Successfully", Toast.LENGTH_SHORT).show()
+                                }
+                                .addOnFailureListener {
+                                    Toast.makeText(holder.itemView.context, "Error ${it.message}", Toast.LENGTH_SHORT).show()
+                                }
+                        }
+                        .setNegativeButton("No"){_,_ ->
+                            Toast.makeText(holder.itemView.context, "Cancelled", Toast.LENGTH_SHORT).show()
+
+                        }
+                        .show() // must call it
+
+                    return@setOnLongClickListener true
+                }
+            }
+        }
+
+    }
+
+}
